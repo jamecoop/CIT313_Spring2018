a6144262201be007ba06fa4c2a00515f07f96796
jamecoop <35437275+jamecoop@users.noreply.github.com>
2018-01-28 23:07:43 -0500

    a1 attempted completion

---
+++ a1/webconfig.php
@@ -0,0 +1,5 @@
+<?php
+//** added absolute path
+define('ABSOLUTE_PATH', '/home/jamecoop/htdocs/CIT313/SP2018/a1');
+//** added URL_ROOT
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:22061/CIT313/SP2018/a1');
